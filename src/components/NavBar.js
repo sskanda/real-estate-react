c0586e7972dd083027c4e6a72fe5b6ca70fd4eb0
Skanda <sskanda98@gmail.com>
2023-06-16 03:47:33 +0530

    added navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../components/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div id="main-container">
+      <span>Navbar</span>
+    </div>
+  );
+};
+
+export default NavBar;
