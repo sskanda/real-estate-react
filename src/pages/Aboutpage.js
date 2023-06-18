a5fe4c7698e14e4080162fa1a43fac03ab84597b
Skanda <sskanda98@gmail.com>
2023-06-19 05:15:32 +0530

    revert change

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -23,6 +23,7 @@ const Aboutpage = () => {
       className={
         sectionvisible ? "animation-hide animation-show" : "animation-hide"
       }
+      ref={myref}
     >
       <div className="about-content">
         <div className="content-header">Explore Our beautiful Homes</div>
@@ -37,7 +38,7 @@ const Aboutpage = () => {
           while our project manager ensures that budget, time and quality goals
           are met.
         </div>
-        <div className="content-button" ref={myref}>
+        <div className="content-button">
           <CustomButton text="View Homes"></CustomButton>
         </div>
       </div>
